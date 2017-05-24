1f1b48b1099cee06be08c15167c1998f47d7854a
Thomas Elliot <thomas.elliot@acquia.com>
2017-05-24 17:02:09 -0400

    test

--- Moonfile.rb
+++ Moonfile.rb
@@ -1,9 +1,8 @@
-APP_NAME = 'phoenix-service'
 
 Moonshot.config do |m|
-  m.app_name = APP_NAME
+  m.app_name = 'lap-tracker'
   m.environment_name = 'dev'
-  m.artifact_repository = S3Bucket.new(APP_NAME)
+  m.artifact_repository = S3Bucket.new('lap-tracker')
   m.build_mechanism = Script.new('moonshot/bin/build.sh')
   m.deployment_mechanism = CodeDeploy.new(asg: 'AutoScalingGroup')
 end
